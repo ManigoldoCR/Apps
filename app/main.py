8dc657bc12f4698eafba2395934320d00234d85b
ManigoldoCR <leo_dbz1@hotmail.com>
2023-05-30 16:38:12 -0300

    Primeiro push

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ janela.config(bg=cor2)
 frame_tela=Frame(janela,width=300,height=50,bg=cor1)
 frame_tela.grid(row=0,column=0)
 
-frame_corpo=Frame(janela,width=300,height=350)
+frame_corpo=Frame(janela,width=300,height=350,)
 
 frame_corpo.grid(row=1,column=0)
 
